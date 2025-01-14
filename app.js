61551b4f52085bf899a55634e9aba0149b215b21
Eddie Tong <edisonjtong@gmail.com>
2025-01-14 14:13:47 -0700

    add turns. contains bug with player movement

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let startingTiles = document.querySelectorAll(".tile.start");
 let displayedFloor = document.querySelector(".board.active");
 let currentFloorBtn = document.querySelector("#current");
 let playerCount = 2;
+let activePlayer;
+let playerIndex;
 
 boards.forEach((board) => {
     board.style.gridTemplateRows = "repeat(11 ,1fr)";
@@ -45,6 +47,7 @@ function positionStartingTiles() {
 }
 
 let players = [];
+
 function positionPlayers(time, floor, rowCol) {
     if (time === "start") {
         for (let i = 1; i <= playerCount; i++) {
@@ -56,6 +59,7 @@ function positionPlayers(time, floor, rowCol) {
                 position: document.createElement("div"),
                 row: 10,
                 col: 6,
+                speed: 4,
             };
 
             player.position.classList.add("player");
@@ -65,22 +69,27 @@ function positionPlayers(time, floor, rowCol) {
             document.querySelector(".board.ground").append(player.position);
             players.push(player);
         }
+        activePlayer = players[0];
+        playerIndex = 0;
         return;
     }
-    players[1].position.remove();
-    players[1].currentFloor = floor;
-    players[1].position.style.gridRow = rowCol[0];
-    players[1].position.style.gridColumn = rowCol[1];
-    displayedFloor.append(players[1].position);
+    activePlayer.position.remove();
+    activePlayer.currentFloor = floor;
+    activePlayer.position.style.gridRow = rowCol[0];
+    activePlayer.position.style.gridColumn = rowCol[1];
+    displayedFloor.append(activePlayer.position);
 }
 
 document.addEventListener("keydown", handlePlayerMovement);
 
 function handlePlayerMovement() {
     let key = event.key;
-    let row = parseInt(players[1].position.style.gridRow);
-    let column = parseInt(players[1].position.style.gridColumn);
-    if (displayedFloor.classList.contains(players[1].currentFloor)) {
+    let row;
+    let column;
+    if (displayedFloor.classList.contains(activePlayer.currentFloor)) {
+        handlePlayerTurns();
+        row = parseInt(activePlayer.position.style.gridRow);
+        column = parseInt(activePlayer.position.style.gridColumn);
         if (key === "ArrowUp") {
             row -= 1;
         } else if (key === "ArrowRight") {
@@ -94,17 +103,17 @@ function handlePlayerMovement() {
             return;
         }
     }
-    players[1].row = row;
-    players[1].col = column;
-    players[1].position.style.gridRow = row;
-    players[1].position.style.gridColumn = column;
+    activePlayer.row = row;
+    activePlayer.col = column;
+    activePlayer.position.style.gridRow = row;
+    activePlayer.position.style.gridColumn = column;
 }
 
 function handlePlayerMovesFloors() {
     if (
         displayedFloor.classList.contains("ground") &&
-        players[1].position.style.gridRow === "8" &&
-        players[1].position.style.gridColumn === "6"
+        activePlayer.position.style.gridRow === "8" &&
+        activePlayer.position.style.gridColumn === "6"
     ) {
         console.log("move to upper");
         switchBoards("upper-btn");
@@ -112,21 +121,36 @@ function handlePlayerMovesFloors() {
     } else if (
         (displayedFloor.classList.contains("upper") ||
             displayedFloor.classList.contains("basement")) &&
-        players[1].position.style.gridRow === "6" &&
-        players[1].position.style.gridColumn === "6"
+        activePlayer.position.style.gridRow === "6" &&
+        activePlayer.position.style.gridColumn === "6"
     ) {
         console.log("move to ground");
         switchBoards("ground-btn");
         positionPlayers("mid", "ground", [8, 6]);
     } else if (
         displayedFloor.classList.contains("ground") &&
-        players[1].position.style.gridRow === "" &&
-        players[1].position.style.gridColumn === "" //no basement stairs location yet
+        activePlayer.position.style.gridRow === "" &&
+        activePlayer.position.style.gridColumn === "" //no basement stairs location yet
     ) {
         console.log("move to basement");
         switchBoards("basement-btn");
         positionPlayers("mid", "basement", [6, 6]);
     }
+}
+
+let thing = true;
+function handlePlayerTurns() {
+    if (activePlayer.speed === 0) {
+        activePlayer.speed = 4;
+        if (thing === true) {
+            activePlayer = players[1];
+            thing = false;
+        } else {
+            activePlayer = players[0];
+            thing = true;
+        }
+    }
+    activePlayer.speed -= 1;
 }
 
 makeButtonsActive();
