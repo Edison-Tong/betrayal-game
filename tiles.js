3945a7a72f9da50c88af7a962daff8009ebc87ab
Edison-Tong <edisonjtong@gmail.com>
2025-01-15 16:56:16 -0700

    updated names of tiles

--- tiles.js
+++ tiles.js
@@ -26,7 +26,7 @@ let initialTiles = [
         col: 6,
     },
     {
-        name: "Foyer",
+        name: "Hallway",
         type: "starting",
         floor: "ground",
         doors: {
@@ -39,7 +39,7 @@ let initialTiles = [
         col: 6,
     },
     {
-        name: "Grand Staircase",
+        name: "Ground Floor Staircase",
         type: "starting",
         floor: "ground",
         doors: {
@@ -68,215 +68,13 @@ let initialTiles = [
 
 let tiles = [
     {
-        name: "Abandoned Room",
-        type: "generic",
-        floors: { basement: true, ground: true, upper: true },
-        doors: { north: true, east: false, south: true, west: true },
-    },
-    {
-        name: "Balcony",
-        type: "generic",
-        floors: { basement: false, ground: false, upper: true },
-        doors: { north: true, east: true, south: false, west: false },
-    },
-    {
-        name: "Chapel",
-        type: "event",
-        floors: { basement: true, ground: true, upper: true },
-        doors: { north: true, east: true, south: false, west: true },
-    },
-    {
-        name: "Charred Room",
-        type: "event",
-        floors: { basement: true, ground: true, upper: false },
-        doors: { north: false, east: true, south: true, west: true },
-    },
-    {
-        name: "Dining Room",
-        type: "item",
-        floors: { basement: false, ground: true, upper: false },
-        doors: { north: true, east: true, south: false, west: true },
-    },
-    {
-        name: "Game Room",
-        type: "event",
-        floors: { basement: false, ground: true, upper: true },
-        doors: { north: true, east: true, south: true, west: false },
-    },
-    {
-        name: "Gardens",
-        type: "event",
-        floors: { basement: false, ground: true, upper: false },
-        doors: { north: true, east: false, south: true, west: true },
-    },
-    {
-        name: "Gymnasium",
-        type: "item",
-        floors: { basement: false, ground: true, upper: true },
-        doors: { north: true, east: true, south: false, west: true },
-    },
-    {
-        name: "Junk Room",
-        type: "event",
-        floors: { basement: true, ground: true, upper: true },
-        doors: { north: false, east: true, south: true, west: true },
-    },
-    {
-        name: "Kitchen",
-        type: "omen",
-        floors: { basement: true, ground: true, upper: false },
-        doors: { north: true, east: true, south: false, west: true },
-    },
-    {
-        name: "Larder",
-        type: "item",
-        floors: { basement: true, ground: false, upper: false },
-        doors: { north: true, east: false, south: false, west: true },
-    },
-    {
-        name: "Library",
-        type: "event",
-        floors: { basement: false, ground: true, upper: true },
-        doors: { north: true, east: true, south: false, west: true },
-    },
-    {
-        name: "Mystic Elevator",
-        type: "special",
-        floors: { basement: true, ground: true, upper: true },
-        doors: { north: true, east: false, south: true, west: true },
-    },
-    {
-        name: "Operating Laboratory",
-        type: "item",
-        floors: { basement: true, ground: true, upper: false },
-        doors: { north: false, east: true, south: true, west: true },
-    },
-    {
-        name: "Organ Room",
-        type: "event",
-        floors: { basement: true, ground: false, upper: true },
-        doors: { north: true, east: false, south: true, west: true },
-    },
-    {
-        name: "Pentagram Chamber",
-        type: "omen",
-        floors: { basement: false, ground: false, upper: true },
-        doors: { north: true, east: false, south: true, west: false },
-    },
-    {
-        name: "Statuary Corridor",
-        type: "item",
-        floors: { basement: true, ground: true, upper: true },
-        doors: { north: true, east: true, south: true, west: true },
-    },
-    {
-        name: "Storeroom",
-        type: "event",
-        floors: { basement: true, ground: true, upper: false },
-        doors: { north: true, east: false, south: true, west: true },
-    },
-    {
-        name: "Tower",
-        type: "item",
-        floors: { basement: false, ground: true, upper: true },
-        doors: { north: true, east: true, south: false, west: true },
-    },
-    {
-        name: "Vault",
-        type: "event",
-        floors: { basement: true, ground: true, upper: true },
-        doors: { north: true, east: true, south: true, west: true },
-    },
-    {
-        name: "Conservatory",
-        type: "event",
-        floors: { basement: false, ground: true, upper: false },
-        doors: { north: true, east: false, south: true, west: true },
-    },
-    {
-        name: "Graveyard",
-        type: "event",
-        floors: { basement: false, ground: true, upper: false },
-        doors: { north: true, east: false, south: false, west: true },
-    },
-    {
-        name: "Patio",
-        type: "event",
-        floors: { basement: false, ground: true, upper: false },
-        doors: { north: true, east: true, south: false, west: true },
-    },
-    {
-        name: "Servants' Quarters",
-        type: "item",
-        floors: { basement: false, ground: true, upper: false },
-        doors: { north: false, east: true, south: true, west: false },
-    },
-    {
-        name: "Wine Cellar",
-        type: "event",
-        floors: { basement: true, ground: false, upper: false },
-        doors: { north: true, east: false, south: true, west: true },
-    },
-    {
-        name: "Collapse Room",
-        type: "event",
-        floors: { basement: true, ground: true, upper: false },
-        doors: { north: false, east: true, south: true, west: true },
-    },
-    {
-        name: "Drawing Room",
-        type: "event",
-        floors: { basement: false, ground: true, upper: false },
-        doors: { north: true, east: true, south: true, west: true },
-    },
-    {
-        name: "Laboratory",
-        type: "item",
-        floors: { basement: true, ground: true, upper: false },
-        doors: { north: true, east: true, south: true, west: false },
-    },
-    {
-        name: "Mudroom",
-        type: "event",
-        floors: { basement: true, ground: true, upper: false },
-        doors: { north: true, east: false, south: true, west: true },
-    },
-    {
-        name: "Attic",
-        type: "item",
-        floors: { basement: false, ground: false, upper: true },
-        doors: { north: true, east: false, south: false, west: true },
-    },
-    {
-        name: "Bedroom",
-        type: "event",
-        floors: { basement: false, ground: false, upper: true },
-        doors: { north: true, east: true, south: false, west: true },
-    },
-    {
-        name: "Bloody Room",
-        type: "omen",
-        floors: { basement: false, ground: false, upper: true },
-        doors: { north: false, east: true, south: true, west: true },
-    },
-    {
-        name: "Master Bedroom",
-        type: "item",
-        floors: { basement: false, ground: false, upper: true },
-        doors: { north: true, east: true, south: false, west: true },
-    },
-    {
-        name: "Nursery",
-        type: "event",
-        floors: { basement: false, ground: false, upper: true },
-        doors: { north: true, east: true, south: false, west: false },
-    },
-    {
-        name: "Basement Stairs",
-        type: "none",
-        floors: { basement: false, ground: true, upper: false },
-        doors: { north: false, east: false, south: true, west: false },
-    },
+        name: "",
+        floors: {basement: true, ground: true, upper: false},
+        doorConfig: "",
+        symbol: "",
+        message: "",
+
+    }
 ];
 
 export { initialTiles, tiles };
